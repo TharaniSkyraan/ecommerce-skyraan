cd3565232d84a20d2137baa382423d8b13e3af11
TharaniSKyraan <tharani@skyraan.com>
2024-07-19 13:06:14 +0530

    update

--- S1k2y3r4a5a6n/app/Http/Livewire/Settings/Settings.php
+++ S1k2y3r4a5a6n/app/Http/Livewire/Settings/Settings.php
@@ -80,8 +80,8 @@ class Settings extends Component
             $validateData
         );
         \Artisan::call('config:clear');
-        \Artisan::call('cache:clear');
-        \Artisan::call('config:cache');
+        // \Artisan::call('cache:clear');
+        // \Artisan::call('config:cache');
         session()->flash('message', 'Updated Successfully.');
 
     }
