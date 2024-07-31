bb5bcd13ed4d9ef632d8ac3a93937571369ed5e8
Pavithra150420 <pavithra@skyraan.com>
2024-07-31 16:16:29 +0530

    changes in marquee

--- S1k2y3r4a5a6n/resources/views/livewire/ecommerce/product/collection-list.blade.php
+++ S1k2y3r4a5a6n/resources/views/livewire/ecommerce/product/collection-list.blade.php
@@ -3,21 +3,22 @@ $baseDuration = 13; $duration = (count($collections) / 5) * $baseDuration;
 @endphp
 <style>
 
-.marquee__group1
+.marquee_content2 .marquee__group1
 {
     animation: scroll1 {{$duration}}s linear infinite;
 }
+
 </style>
-<section class="marquee_content2 marquee_content">
-    <div class="marquee py-4" >
+<section class="marquee_content2">
+    <div class="marquee py-4">
         <div class="marquee__group1">
             @foreach($collections as $collection)
                 @if(empty($collection['product_slug']))
-                    <a href="{{ route('ecommerce.product.list', ['type' => 'collection', 'slug' => $collection['slug']]) }}">
+                    <a href="{{ route('ecommerce.product.list', ['type' => 'collection', 'slug' => $collection['slug']]) }}" >
                         <img src="{{ asset('storage') }}/{{$collection['image']}}" alt="{{$collection['name']}}">
                     </a>
                 @else
-                    <a href="{{ route('ecommerce.product.detail', ['slug' => $collection['product_slug']]) }}?prdRef={{ \Carbon\Carbon::parse($collection['product_created'])->timestamp }}">
+                    <a href="{{ route('ecommerce.product.detail', ['slug' => $collection['product_slug']]) }}?prdRef={{ \Carbon\Carbon::parse($collection['product_created'])->timestamp }}" >
                         <img src="{{ asset('storage') }}/{{$collection['image']}}" alt="{{$collection['name']}}">
                     </a>
                 @endif
@@ -26,11 +27,11 @@ $baseDuration = 13; $duration = (count($collections) / 5) * $baseDuration;
         <div aria-hidden="true" class="marquee__group1">
             @foreach($collections as $collection)
                 @if(empty($collection['product_slug']))
-                    <a href="{{ route('ecommerce.product.list', ['type' => 'collection', 'slug' => $collection['slug']]) }}">
+                    <a href="{{ route('ecommerce.product.list', ['type' => 'collection', 'slug' => $collection['slug']]) }}" >
                         <img src="{{ asset('storage') }}/{{$collection['image']}}" alt="{{$collection['name']}}">
                     </a>
                 @else
-                    <a href="{{ route('ecommerce.product.detail', ['slug' => $collection['product_slug']]) }}?prdRef={{ \Carbon\Carbon::parse($collection['product_created'])->timestamp }}">
+                    <a href="{{ route('ecommerce.product.detail', ['slug' => $collection['product_slug']]) }}?prdRef={{ \Carbon\Carbon::parse($collection['product_created'])->timestamp }}" >
                         <img src="{{ asset('storage') }}/{{$collection['image']}}" alt="{{$collection['name']}}">
                     </a>
                 @endif
