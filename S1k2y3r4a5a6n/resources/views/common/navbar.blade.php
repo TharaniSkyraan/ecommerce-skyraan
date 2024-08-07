8e85b2c029e8cc7d86ad122ae8a901e103121cea
Pavithra150420 <pavithra@skyraan.com>
2024-08-07 10:44:28 +0530

    update

--- S1k2y3r4a5a6n/resources/views/common/navbar.blade.php
+++ S1k2y3r4a5a6n/resources/views/common/navbar.blade.php
@@ -37,7 +37,15 @@
     </div>
 
     <!-- Add to cart offcanvas left -->
-    @livewire('ecommerce.layout.view-canvas-cart')  
+    
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+        <div class="offcanvas-header">
+            <h6 class="offcanvas-title fw-bold" id="offcanvasRightLabel">My Shopping Cart</h6>
+            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        @livewire('ecommerce.layout.view-canvas-cart')  
+              
+    </div>
 
     <!-- Sign in Modal -->
     @if(!Auth::check())
@@ -177,5 +185,4 @@
             </div>
         </div>
     </div>  
-    
 </section>
