f538d50dfda4a7bb8ff6fc65c8ee389987842b28
TharaniSKyraan <tharani@skyraan.com>
2024-08-08 13:20:45 +0530

    update

--- S1k2y3r4a5a6n/app/Http/Middleware/Locale.php
+++ S1k2y3r4a5a6n/app/Http/Middleware/Locale.php
@@ -4,111 +4,124 @@ namespace App\Http\Middleware;
 
 use App\Traits\GeoIPService;
 use App\Traits\ZoneConfig;
-use Closure, Session, View, App\Models\SavedAddress, App\Models\Zone;
-use Auth;
-use Cookie;
+use Closure;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\View;
+use App\Models\Zone;
+use Illuminate\Support\Facades\Auth;
 
 class Locale
 {
-
     use GeoIPService, ZoneConfig;
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  $guard
-     * @return mixed
-     */
-    public function handle($request, Closure $next, $guard = null)
+
+    public function handle($request, Closure $next)
     {
-        if(Session::has('zone_config')){
-            $zoneData = \Session::get('zone_config');
-            if(!empty($zoneData['zone_id'])){
-                
-                $zonesdata = Zone::where('id',$zoneData['zone_id'])->whereStatus('active')->first();
-
-                if(isset($zonesdata) && ($zoneData['warehouse_ids'] == $zonesdata->warehouse_ids))
-                {
-                    view()->share('zone_data',Session::get('zone_config'));
-                }
-            }
-            // Session::forget('zone_config');
-
-        }
-        if(Session::has('zone_config')==false || empty(Session::get('zone_config')['postal_code']))
-        {  
-
-            if(Auth::check() && (isset(Auth::user()->address) || isset(Auth::user()->usercart->address)))
-            {
-                $address = Auth::user()->usercart->address??Auth::user()->address;
-
-                $ipLocationData = array(
-                    'address_id' => $address->id??'',
-                    'city' => $address->city??'',
-                    'latitude' => '',
-                    'longitude' => '',
-                    'postal_code' => $address->postal_code??''
-                );  
-                if($ipLocationData && $ipLocationData!=null)
-                {
-                    $result = $this->configzone($ipLocationData); 
-                    session(['zone_config' => $result]);
-                    view()->share('zone_data',\Session::get('zone_config'));
-                }
-            }else{
-                
-                $ip = $request->ip();    
-
-                $ipLocationData = $this->getCity('183.82.250.192');    
-                if($ipLocationData == null || empty($ipLocationData['postal_code'])){
-                    $ipLocationData = @json_decode(file_get_contents("https://ipinfo.io/".'183.82.250.192'));  
-                    if($ipLocationData && $ipLocationData!=null)
-                    {
-                        $ipLocationData = array(
-                            'address_id' => '',
-                            'city' => $ipLocationData->city??'',
-                            'latitude' => (isset($ipLocationData->loc))?explode(',',$ipLocationData->loc)[0]:'',
-                            'longitude' => (isset($ipLocationData->loc))?explode(',',$ipLocationData->loc)[1]:'',
-                            'postal_code' => $ipLocationData->postal??''
-                        );  
-                    }   
-                }
-                
-                if($ipLocationData && $ipLocationData!=null)
-                {
-                    $result = $this->configzone($ipLocationData); 
-                    session(['zone_config' => $result]);
-                    view()->share('zone_data',\Session::get('zone_config'));
-                }
-            }
-
-        }
-        
-        if(Session::has('ip_config')==false)
-        {
-            $ip = $request->ip();   
-       
-            $ipData = $this->getCity('183.82.250.192');   
-            if($ipData == null || empty($ipData['country_code'])){  
-                $ipData = @json_decode(file_get_contents("https://ipinfo.io/".'183.82.250.192'));    
-                if($ipData && $ipData!=null)
-                {
-                    $ipData = array('country_code' => $ipData->geoplugin_countryCode??''); 
-                }
-            }
-            
-            if($ipData && $ipData!=null)
-            {
-                $country = \App\Models\Country::where('code',$ipData['country_code']??'IN')->first();
-                session(['ip_config' => $country]);
-                view()->share('ip_data',Session::get('ip_config'));
-            }
-        }else{
-            view()->share('ip_data',Session::get('ip_config'));
-        }   
+        $this->handleZoneConfig($request);
+        $this->handleIpConfig($request);
 
         return $next($request);
     }
 
+    protected function handleZoneConfig($request)
+    {
+        $zone_config = Session::has('zone_config');
+        $zone_data = Session::get('zone_config');
+        if ($zone_config) {
+            if (!empty($zone_data['zone_id'])) {
+                $zones = Zone::where('id',$zone_data['zone_id'])->whereStatus('active')->first();
+                if (isset($zone) && ($zone_data['warehouse_ids'] == $zones->warehouse_ids)) {
+                    View::share('zone_data', $zone_data);
+                }
+                else{$zone_config = false;}
+            }
+            else{$zone_config = false;}
+        }
+
+        if (!$zone_config || empty($zone_data['postal_code'])) {
+            $address = $this->getUserAddress();
+            if ($address) {
+                $ipLocationData = $this->getIpLocationDataFromAddress($address);
+                $this->setZoneConfig($ipLocationData);
+            } else {
+                $ip ='183.82.250.192';   
+                // $ip = $request->ip();  
+                $ipLocationData = $this->getIpLocationDataFromIp($ip);
+                $this->setZoneConfig($ipLocationData);
+            }
+        }
+    }
+
+    protected function handleIpConfig($request)
+    {
+        if (!Session::has('ip_config')) {
+            $ip ='183.82.250.192';   
+            // $ip = $request->ip();   
+            $ipData = $this->getIpData($ip);
+            if ($ipData) {
+                $country = \App\Models\Country::where('code', $ipData['country_code'] ?? 'IN')->first();
+                Session::put('ip_config', $country);
+                View::share('ip_data', $country);
+            }
+        } else {
+            View::share('ip_data', Session::get('ip_config'));
+        }
+    }
+
+    protected function getUserAddress()
+    {
+        if (Auth::check()) {
+            return Auth::user()->usercart->address ?? Auth::user()->address;
+        }
+        return null;
+    }
+
+    protected function getIpLocationDataFromAddress($address)
+    {
+        return [
+            'address_id' => $address->id ?? '',
+            'city' => $address->city ?? '',
+            'latitude' => '',
+            'longitude' => '',
+            'postal_code' => $address->postal_code ?? ''
+        ];
+    }
+
+    protected function getIpLocationDataFromIp($ip)
+    {
+        $ipLocationData = $this->getCity($ip);
+        if (!$ipLocationData || empty($ipLocationData['postal_code'])) {
+            $ipLocationData = @json_decode(file_get_contents("https://ipinfo.io/{$ip}"));
+            if ($ipLocationData) {
+                $ipLocationData = [
+                    'address_id' => '',
+                    'city' => $ipLocationData->city ?? '',
+                    'latitude' => explode(',', $ipLocationData->loc)[0] ?? '',
+                    'longitude' => explode(',', $ipLocationData->loc)[1] ?? '',
+                    'postal_code' => $ipLocationData->postal ?? ''
+                ];
+            }
+        }
+        return $ipLocationData;
+    }
+
+    protected function setZoneConfig($ipLocationData)
+    {
+        if ($ipLocationData) {
+            $result = $this->configzone($ipLocationData);
+            Session::put('zone_config', $result);
+            View::share('zone_data', $result);
+        }
+    }
+
+    protected function getIpData($ip)
+    {
+        $ipData = $this->getCity($ip);
+        if (!$ipData || empty($ipData['country_code'])) {
+            $ipData = @json_decode(file_get_contents("https://ipinfo.io/{$ip}"));
+            if ($ipData) {
+                $ipData = ['country_code' => $ipData->country ?? ''];
+            }
+        }
+        return $ipData;
+    }
 }
