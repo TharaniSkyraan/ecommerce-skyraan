4b5b7f649df8d9a13e5e8fe45aea5ad4be200ba7
Pavithra150420 <pavithra@skyraan.com>
2024-08-13 18:14:32 +0530

    chnages

--- S1k2y3r4a5a6n/app/Http/Livewire/Ecommerce/User/Auth/Orders.php
+++ S1k2y3r4a5a6n/app/Http/Livewire/Ecommerce/User/Auth/Orders.php
@@ -127,8 +127,7 @@ class Orders extends Component
         $this->reset(['order_id','reason', 'notes']);  
 
         // Order cancelled mail
-        $this->order_code = $order_code;
-        $order= Order::where('code',$order_code)->first();
+        $order= Order::find($this->order_id);
         \Mail::send(new OrderCancelMail($order));
 
         if(!empty($order->payments->charge_id)){
