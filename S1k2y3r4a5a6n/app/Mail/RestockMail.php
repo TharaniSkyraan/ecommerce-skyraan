1369721d9914c544a17b8453af49966c5009c6ae
TharaniSKyraan <tharani@skyraan.com>
2024-08-14 12:43:14 +0530

    udpate

--- S1k2y3r4a5a6n/app/Mail/RestockMail.php
+++ S1k2y3r4a5a6n/app/Mail/RestockMail.php
@@ -3,51 +3,32 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class RestockMail extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public $data;
+
     /**
      * Create a new message instance.
      */
-    public function __construct()
+    public function __construct($data)
     {
-        //
+        $this->data = $data;
     }
-
-    /**
-     * Get the message envelope.
-     */
-    public function envelope(): Envelope
-    {
-        return new Envelope(
-            subject: 'Restock Mail',
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'view.name',
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
+    
     public function build()
     {
-        return $this->markdown('emails.restack');
-    }   
+        return $this->from(config('mail.recieve_to.address'), config('mail.recieve_to.name'))
+                    ->to($this->email, $this->name)
+                    ->subject('Forgotten Cart at ' . config('siteSetting.site_name'))
+                    ->markdown('emails.restack')
+                    ->with([
+                        'name' => $this->name,
+                        'product' => $this->product,
+                    ]);
+    }
 }
